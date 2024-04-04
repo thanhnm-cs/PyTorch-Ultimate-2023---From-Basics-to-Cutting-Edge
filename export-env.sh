7e9fa5854d3b0ed65e7d36e080486637b86f1ff1
thanh <thanhnm777@gmail.com>
2024-04-04 15:36:57 +0700

    update

---
+++ export-env.sh
@@ -0,0 +1,1 @@
+conda env export | grep -v "^prefix: " > pytorch.yml
---
+++ install.sh
@@ -0,0 +1,7 @@
+conda create -n pytorch python=3.10
+conda activate pytorch
+conda install pytorch torchvision torchaudio cpuonly -c pytorch
+conda install ipykernel
+conda install -c anaconda seaborn
+conda install scikit-learn
+conda install -c conda-forge detecto
