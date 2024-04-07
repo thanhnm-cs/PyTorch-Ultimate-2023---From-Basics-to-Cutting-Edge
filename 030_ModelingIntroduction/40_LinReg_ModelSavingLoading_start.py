abbfc5898fe6dfdfe2d48b89562cd8606bdac6dc
thanh <thanhnm777@gmail.com>
2024-04-07 16:17:44 +0700

    END OF 1 - 7

--- 030_ModelingIntroduction/40_LinReg_ModelSavingLoading_start.py
+++ 030_ModelingIntroduction/40_LinReg_ModelSavingLoading_start.py
@@ -6,6 +6,8 @@ import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 import seaborn as sns
 
+from skorch import NeuralNetRegressor
+
 # %% data import
 cars_file = "https://gist.githubusercontent.com/noamross/e5d3e859aa0c794be10b/raw/b999fb4425b54c63cab088c0ce2c0d6ce961a563/cars.csv"
 cars = pd.read_csv(cars_file)
